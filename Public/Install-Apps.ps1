e4c2026f4be5d8af1adb3093b9aef9f971784508
Edditoria <edditoria@pm.me>
2025-05-29 00:08:34 +0800

    Public: Install-Apps: Add script file to install apps

---
+++ Public/Install-Apps.ps1
@@ -0,0 +1,10 @@
+choco install ChocolateyGUI --yes
+choco install 7zip.install --yes
+choco install mise --yes
+choco pin add --name="'mise'" --yes
+choco install notepadplusplus.install --yes
+choco install git.install --yes
+choco install lazygit --yes
+choco install vscode.install --yes
+choco pin add --name="'vscode'" --yes
+choco install meld --yes
